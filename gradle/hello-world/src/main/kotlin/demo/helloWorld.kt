8b7805d18137cfbbd3438358d46d56b4b0cdc747
Kyle Kauffman <kyle@kyonifer.com>
2021-05-10 15:43:55 -0400

    Initial commit.

--- gradle/hello-world/src/main/kotlin/demo/helloWorld.kt
+++ gradle/hello-world/src/main/kotlin/demo/helloWorld.kt
@@ -1,13 +1,16 @@
 package demo
 
+import koma.*
+
 fun getGreeting(): String {
     val words = mutableListOf<String>()
     words.add("Hello,")
-    words.add("world!")
+    words.add("koma world!")
 
     return words.joinToString(separator = " ")
 }
 
 fun main(args: Array<String>) {
     println(getGreeting())
+    println(koma.randn(5,5))
 }
